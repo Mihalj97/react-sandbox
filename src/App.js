ef614a2b961b2c4af14606d4b62dda082f60898a
Mihalj97 <mihalj.tandi5@gmail.com>
2022-02-09 15:21:57 +0100

    useRef Example 3 - Memory Leak Error Fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import UseRefExample from './components/UseRefExample';
-import UseRefExample2 from './components/UseRefExample2'
+import UseRefExample3 from './components/UseRefExample3'
 
 function App() {
   return (
     <div>
-      <UseRefExample2 />
+      <UseRefExample3 />
     </div>
   )
 }
